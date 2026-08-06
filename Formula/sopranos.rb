5579631c58964366ebdb18555bd3e06b40398124
noahfinkelstein <noah_finkelstein@brown.edu>
2026-08-05 20:15:03 -0400

    Point formula at v1.4.0

--- Formula/sopranos.rb
+++ Formula/sopranos.rb
@@ -1,8 +1,8 @@
 class Sopranos < Formula
   desc "Sopranos quotes in your terminal, with ASCII portraits"
   homepage "https://github.com/noahfinkelstein/homebrew-tonysoprano"
-  url "https://github.com/noahfinkelstein/homebrew-tonysoprano/archive/refs/tags/v1.3.1.tar.gz"
-  sha256 "d2ae225491a75aea49d0a5949eda5455a34c9887183dd3764860165476491317"
+  url "https://github.com/noahfinkelstein/homebrew-tonysoprano/archive/refs/tags/v1.4.0.tar.gz"
+  sha256 "8ad7e6bc813a3779b1f6cc7990bb16655c40192629ca3fa18c190b047e1453f9"
   license "MIT"
   head "https://github.com/noahfinkelstein/homebrew-tonysoprano.git", branch: "main"
 
